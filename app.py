41e2a49584ec27efb6dfa5a12301367be333d4d8
Chiara Palma <chiara.palma@student-cs.fr>
2022-06-15 09:53:42 +0200

    small change

--- app.py
+++ app.py
@@ -161,8 +161,8 @@ else:
                         "Admission": "by admission",
                         "Admission AST": "by admission (AST tracks all combined)"}
 
-#group = st.selectbox("Grouping to visualize", groups, index=0,format_func=lambda x: formatting_group[x])
-group = st.selectbox("Grouping to visualize", groups, index=0)
+group = st.selectbox("Grouping to visualize", groups, index=0,format_func=lambda x: formatting_group[x])
+#group = st.selectbox("Grouping to visualize", groups, index=0)
 decimals = st.number_input("Decimal positions to visualize?", min_value=0, max_value=None, value=2, step=1)
 weighted = st.checkbox("Visualize weighted partial scores")
 scores = factors.score(df_factors, group, weights, na_method,weighted)
